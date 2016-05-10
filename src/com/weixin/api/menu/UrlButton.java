b03e0b21f2bd68169dfeb70a78e6ea0114e085b6
tombsky <tombsky@163.com>
2016-05-10 23:39:07 +0800

    修改菜单错误

--- src/com/weixin/api/menu/UrlButton.java
+++ src/com/weixin/api/menu/UrlButton.java
@@ -5,7 +5,7 @@ package com.weixin.api.menu;
  * @date     创建时间：2015年4月15日 下午5:14:34
  * @version 1.0
  */
-public class UrlButton {
+public class UrlButton extends Button {
 	//菜单的响应动作类型
 	private String type;
 	//网页链接，用户点击菜单可打开链接，不超过256字节
